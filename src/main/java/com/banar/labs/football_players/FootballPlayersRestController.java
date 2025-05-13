3ef13e1f947a84ab9aac101d1880164e8fed9f99
DmytroBanar <banar.dima04@gmail.com>
2025-05-13 18:55:02 +0300

    Change atribute from role to auth

--- src/main/java/com/banar/labs/football_players/FootballPlayersRestController.java
+++ src/main/java/com/banar/labs/football_players/FootballPlayersRestController.java
@@ -13,49 +13,49 @@ public class FootballPlayersRestController {
     private final FootballPlayersService footballPlayersService;
 
     @GetMapping
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPERADMIN')")
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN', 'SUPERADMIN')")
     public List<FootballPlayers> showAll() {
         return footballPlayersService.getAll();
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPERADMIN')")
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN', 'SUPERADMIN')")
     public FootballPlayers showOneById(@PathVariable String id) {
         return footballPlayersService.getById(id);
     }
 
     @PostMapping
-    @PreAuthorize("hasAnyRole('ADMIN', 'SUPERADMIN')")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPERADMIN')")
     public FootballPlayers insert(@RequestBody FootballPlayers FootballPlayers) {
         return footballPlayersService.create(FootballPlayers);
     }
 
     @PutMapping
-    @PreAuthorize("hasAnyRole('ADMIN', 'SUPERADMIN')")
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'SUPERADMIN')")
     public FootballPlayers edit(@RequestBody FootballPlayers FootballPlayers) {
         return footballPlayersService.update(FootballPlayers);
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("hasAnyRole('SUPERADMIN')")
+    @PreAuthorize("hasAnyAuthority('SUPERADMIN')")
     public void delete(@PathVariable String id) {
         footballPlayersService.delById(id);
     }
 
     @GetMapping("/age/{minAge}/{maxAge}")
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPERADMIN')")
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN', 'SUPERADMIN')")
     public List<FootballPlayers> getByAgeRange(@PathVariable int minAge, @PathVariable int maxAge) {
         return footballPlayersService.getByAgeRange(minAge, maxAge);
     }
 
     @GetMapping("/team/{team}")
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPERADMIN')")
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN', 'SUPERADMIN')")
     public List<FootballPlayers> getByTeam(@PathVariable String team) {
         return footballPlayersService.getByTeam(team);
     }
 
     @GetMapping("/country/{country}")
-    @PreAuthorize("hasAnyRole('USER', 'ADMIN', 'SUPERADMIN')")
+    @PreAuthorize("hasAnyAuthority('USER', 'ADMIN', 'SUPERADMIN')")
     public List<FootballPlayers> getByCountry(@PathVariable String country) {
         return footballPlayersService.getByCountry(country);
     }
